464540d41821e971397537006fcd8c6edb32bb07
gpaparaz <giorgia.paparazzo@gmail.com>
2023-02-10 18:54:31 +0100

    add home in menu

--- js/reusable-items.js
+++ js/reusable-items.js
@@ -4,9 +4,10 @@ const htmlMenu = `<div class="container">
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
-<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="collapse navbar-collapse px-5" id="navbarNavAltMarkup">
   <div class="navbar-nav">
-    <a class="nav-link" aria-current="page" href="1_mission.html">MISSION</a>
+    <a class="nav-link" href="index.html">HOME</a>
+    <a class="nav-link" href="1_mission.html">MISSION</a>
     <a class="nav-link" href="2_prodotti.html">PRODOTTI</a>
     <a class="nav-link" href="3_about.html">ABOUT</a>
     <a class="nav-link" href="4_blog.html">BLOG</a>
