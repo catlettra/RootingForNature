9a5ae185a84eeb0257f6bc75947da92f54a89831
gpaparaz <giorgia.paparazzo@gmail.com>
2023-02-10 19:14:41 +0100

    add animation in blog

--- js/animations.js
+++ js/animations.js
@@ -1,15 +1,5 @@
 $(document).ready(function (){
 
-    // $(window).scroll(function () {
-    //     $('.hero-text').each(function (i) {
-    //         var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
-    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //         if (bottom_of_window > bottom_of_object) {
-    //             $(this).addClass('animate__animated animate__fadeInDown');
-    //         }
-    //     })
-    // });
-
     $(window).scroll(function () {
         $('.section-w').each(function (i) {
             var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
@@ -26,6 +16,17 @@ $(document).ready(function (){
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if (bottom_of_window > bottom_of_object) {
                 $(this).addClass('animate__animated animate__swing');
+            }
+        })
+    });
+
+    $(window).scroll(function () {
+        $('.title-leaf').each(function (i) {
+            var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).removeClass('opacity-0');
+                $(this).addClass('animate__animated animate__fadeIn');
             }
         })
     });
