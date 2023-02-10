b9d2297f4479820ed29c52901a4f3fc73a84d090
gpaparaz <giorgia.paparazzo@gmail.com>
2023-02-10 19:07:45 +0100

    fix animations

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,14 @@
 $(document).ready(function (){
 
-    $(window).scroll(function () {
-        $('.hero-text').each(function (i) {
-            var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if (bottom_of_window > bottom_of_object) {
-                $(this).addClass('animate__animated animate__fadeInDown');
-            }
-        })
-    });
+    // $(window).scroll(function () {
+    //     $('.hero-text').each(function (i) {
+    //         var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
+    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //         if (bottom_of_window > bottom_of_object) {
+    //             $(this).addClass('animate__animated animate__fadeInDown');
+    //         }
+    //     })
+    // });
 
     $(window).scroll(function () {
         $('.section-w').each(function (i) {
